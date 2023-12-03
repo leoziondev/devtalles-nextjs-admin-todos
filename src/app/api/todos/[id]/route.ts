c17475c9dce36a9797d4219fff15900146fd849d
leoziondev <leozionsuporte@gmail.com>
2023-12-03 13:35:15 -0300

    update validate and optimize duplicate cod

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
+import { Todo } from "@prisma/client";
 import { NextResponse } from "next/server";
+import * as yup from "yup";
 
 interface Segments {
     params: {
@@ -7,31 +9,44 @@ interface Segments {
     }
 }
 
-export async function GET(request: Request, { params }: Segments) {
-    const { id } = params
+const getTodo = async(id: string): Promise<Todo | null> => {
     const todo = await prisma.todo.findFirst({ where: { id } })
 
+    return todo
+}
+
+export async function GET(request: Request, { params }: Segments) {
+    const todo = await getTodo(params.id)
+
     if (!todo) {
-        return NextResponse.json({ message: `Todo id: ${id} doesnt exist!` }, { status: 400 })
+        return NextResponse.json({ message: `Todo id: ${params.id} doesnt exist!` }, { status: 400 })
     }
 
     return NextResponse.json(todo)
 }
 
+const putSchema = yup.object({
+    complete: yup.boolean().optional(),
+    description: yup.string().optional()
+})
+
 export async function PUT(request: Request, { params }: Segments) {
-    const { id } = params
-    const todo = await prisma.todo.findFirst({ where: { id } })
+    const todo = await getTodo(params.id)
 
     if (!todo) {
-        return NextResponse.json({ message: `Todo id: ${id} doesnt exist!` }, { status: 400 })
+        return NextResponse.json({ message: `Todo id: ${params.id} doesnt exist!` }, { status: 400 })
     }
 
-    const body = await request.json()
+    try {
+        const { complete, description } = await putSchema.validate(await request.json())
 
-    const updatedTodo = await prisma.todo.update({
-        where: { id },
-        data: { ...body}
-    })
+        const updatedTodo = await prisma.todo.update({
+            where: { id: params.id },
+            data: { complete, description }
+        })
 
-    return NextResponse.json(updatedTodo)
+        return NextResponse.json(updatedTodo)
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 })
+    }    
 }
