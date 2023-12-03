6e151ddd9cbc36ab14ce98c2a12c29f4fa5e3951
leoziondev <leozionsuporte@gmail.com>
2023-12-03 12:24:57 -0300

    simple pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,8 +2,19 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url)
+    const take = Number(searchParams.get('take') ?? '10')
+    const skip = Number(searchParams.get('skip') ?? '0')
 
-    const todos = await prisma.todo.findMany()
+    if (isNaN(take)) {
+        return NextResponse.json({ message: 'Take it must be a number' }, { status: 400 })
+    }
+
+    if (isNaN(skip)) {
+        return NextResponse.json({ message: 'Skip it must be a number' }, { status: 400 })
+    }
+
+    const todos = await prisma.todo.findMany({ take, skip })
 
     return NextResponse.json(todos)
 }
