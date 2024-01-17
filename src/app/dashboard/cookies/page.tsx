01d1a8551f9dd7b2d7a963d9d9dbeb4c34074195
leoziondev <leozionsuporte@gmail.com>
2024-01-17 20:51:25 -0300

    cookies server side

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from "@/components";
+import { cookies } from "next/headers";
 
 export const metadata = {
  title: 'Cookies Page',
@@ -6,12 +7,15 @@ export const metadata = {
 };
 
 export default function CookiesPage() {
+    const cookieStore = cookies()
+    const cookieTab = cookieStore.get('selectedTab')?.value ?? '1'
+
   return (
     <div className="grid sm:grid-cols-2 gap-3">
         <div className="flex flex-col">
             <span className="text-3xl font-bold mb-10 block">Tabs</span>
     
-            <TabBar />
+            <TabBar currentTab={ +cookieTab } />
         </div>
     </div>
   )
