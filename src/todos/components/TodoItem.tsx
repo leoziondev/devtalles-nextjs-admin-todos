51e2c86cc2ff0a860121e5b83c97f212466f8e9a
leoziondev <leozionsuporte@gmail.com>
2024-01-17 02:30:28 -0300

    useOptimistic hook

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,7 +1,9 @@
+import { startTransition, useOptimistic } from "react"
 import { Todo } from "@prisma/client"
 
 import styles from './TodoItem.module.css'
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5"
+import { boolean } from "yup"
 
 interface Props {
     todo: Todo
@@ -9,26 +11,42 @@ interface Props {
 }
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
-  return (
-    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
-        <div className="flex justify-start items-center gap-4">
-            <div 
-                onClick={() => toggleTodo(todo.id, !todo.complete)}
-                className={`
-                flex p-2 rounded-md cursor-pointer
-                hover:bg-opacity-60
-                ${todo.complete ? 'bg-blue-100' : 'bg-red-100'}`}
-            >
-                {todo.complete ? (
-                    <IoCheckboxOutline size={30} />
-                ) : (
-                    <IoSquareOutline size={30} />
-                )}
-            </div>
-            <div>
-                {todo.description}
+    const [todoOptimistic, toggleTodoOptimistic] = useOptimistic(
+        todo,
+        (state, newCompleteValue: boolean) => ({ ...state, complete: newCompleteValue })
+    )
+
+    const onToggleTodo = async () => {
+        try {
+            startTransition(() => toggleTodoOptimistic(!todoOptimistic.complete))
+            
+            await toggleTodo( todoOptimistic.id, !todoOptimistic.complete )
+        } catch (error) {
+            startTransition(() => toggleTodoOptimistic(!todoOptimistic.complete))
+        }
+    }
+
+    return (
+        <div className={todoOptimistic.complete ? styles.todoDone : styles.todoPending}>
+            <div className="flex justify-start items-center gap-4">
+                <div 
+                    // onClick={() => toggleTodo(todoOptimistic.id, !todoOptimistic.complete)}
+                    onClick={onToggleTodo}
+                    className={`
+                    flex p-2 rounded-md cursor-pointer
+                    hover:bg-opacity-60
+                    ${todoOptimistic.complete ? 'bg-blue-100' : 'bg-red-100'}`}
+                >
+                    {todoOptimistic.complete ? (
+                        <IoCheckboxOutline size={30} />
+                    ) : (
+                        <IoSquareOutline size={30} />
+                    )}
+                </div>
+                <div>
+                    {todoOptimistic.description}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
