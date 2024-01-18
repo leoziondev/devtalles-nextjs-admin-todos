51ba06734e5671545c968aea5857562191d39ded
leoziondev <leozionsuporte@gmail.com>
2024-01-18 11:51:34 -0300

    total cart and tax

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import { products, type Product } from "@/products/data/products";
 
 import { ItemCard } from "@/shopping-cart/components/ItemCart";
+import { WidgetItem } from "@/components";
 
 export const metadata = {
  title: 'Products Cart',
@@ -33,6 +34,9 @@ export default function CartPage() {
   const cart = JSON.parse( cookieStore.get('cart')?.value ?? '{}' ) as { [id: string]: number }
   const productsInCart = getProductsInCart(cart)
 
+  const totalToPay = productsInCart.reduce(
+    ( prev, current ) => ( current.product.price * current.quantity ) + prev, 0)
+
   return (
     <div>
       <span className="text-3xl font-bold mb-10 block">Cart Products</span>
@@ -46,6 +50,15 @@ export default function CartPage() {
               quantity={quantity}
             />
           ))}
+        </div>
+
+        <div className="flex flex-col w-full sm:w-4/12">
+          <WidgetItem title="Total Cart">
+            <div className="mt-2 flex justify-center">
+              <h3 className="text-3xl font-bold text-slate-700">$ {(totalToPay * 1.15).toFixed(2)}</h3>
+            </div>
+            <span className="font-bold text-center text-slate-500">TAX 15% $ {(totalToPay * 0.15).toFixed(2)}</span>
+          </WidgetItem>
         </div>
       </div>
     </div>
